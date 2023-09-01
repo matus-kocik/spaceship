329d83c54cc718e3ebd7f4d82e13e431d4c053d8
Matúš Kočik <matuskocik@gmail.com>
2023-09-01 18:23:15 +0200

    add settings.py

--- game.py
+++ game.py
@@ -1,22 +1,25 @@
 import pygame
 import sys
+from settings import Settings
 
 class Game:
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Spaceship")
         self.clock = pygame.time.Clock()
 
     def run(self):
         
-        
+     
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
             self.clock.tick(60)
             
